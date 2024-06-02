6cce3370b48b990594760cd4f65efc6c2dfc94d0
WilliamNW <williamnw943@gmail.com>
2024-06-02 22:27:40 +0700

    change directory name

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // Pages
-import StartPage from "./pages/StartPage.jsx";
+import StartPage from "./pages/startPage.jsx";
 import GroupName from "./pages/GroupName.jsx";
 import MemberName from "./pages/MemberName.jsx";
 import ItemList from "./pages/ItemList.jsx";
